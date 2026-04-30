3799be70b92aabbc6f4c984d3e69954b7039e0cb
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-04-30 13:48:23 +0200

    add user input

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ namespace FlowControl
     {
         static void Main (string[] argv)
         {
+            Console.WriteLine("");
             Console.WriteLine("Welcome to the Main Menu!");
             Console.WriteLine("--------------------------");
             Console.WriteLine("Please use 0-9 to navigate");
             Console.WriteLine("\n");
             Console.WriteLine("0. Quit");
+            Console.WriteLine("");
+            Console.Write("Input: ");
+            string? input = Console.ReadLine();
         }
     }
 }
