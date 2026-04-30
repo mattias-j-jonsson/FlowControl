1726aff2aecc98fabfef6d00e0db1e492873cf1d
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-04-30 13:44:47 +0200

    print first version of menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace FlowControl
+{
+    internal class Program
+    {
+        static void Main (string[] argv)
+        {
+            Console.WriteLine("Welcome to the Main Menu!");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Please use 0-9 to navigate");
+            Console.WriteLine("\n");
+            Console.WriteLine("0. Quit");
+        }
+    }
+}
